78639689690b2a6d9c42c2d62ff668b9f7f94740
David Carter <indysoftwaredev@gmail.com>
2023-10-04 19:04:18 -0400

    Different ways of Asserting that a value is in a range.

--- EmployeeManagement/EmployeeManagement.Test/EmployeeFactoryTests.cs
+++ EmployeeManagement/EmployeeManagement.Test/EmployeeFactoryTests.cs
@@ -17,5 +17,30 @@ namespace EmployeeManagement.Test
             var employee = (InternalEmployee)employeeFactory.CreateEmployee("Kevin", "Dockx");
             Assert.Equal(2500, employee.Salary);
         }
+
+        [Fact]
+        public void CreateEmployee_ConstructInternalEmployee_SalaryMustBeBetween2500And3500()
+        {
+            var employeeFactory = new EmployeeFactory();
+            var employee = (InternalEmployee)employeeFactory.CreateEmployee("Kevin", "Dockx");
+            Assert.True(employee.Salary >= 2500 && employee.Salary <= 3500, "Salary not in acceptable range.");
+        }
+
+        [Fact]
+        public void CreateEmployee_ConstructInternalEmployee_SalaryMustBeBetween2500And3500_Alternative()
+        {
+            var employeeFactory = new EmployeeFactory();
+            var employee = (InternalEmployee)employeeFactory.CreateEmployee("Kevin", "Dockx");
+            Assert.True(employee.Salary >= 2500, "Salary not in acceptable range.");
+            Assert.True(employee.Salary <= 3500, "Salary not in acceptable range.");
+        }
+
+        [Fact]
+        public void CreateEmployee_ConstructInternalEmployee_SalaryMustBeBetween2500And3500_AlternativeWithinRange()
+        {
+            var employeeFactory = new EmployeeFactory();
+            var employee = (InternalEmployee)employeeFactory.CreateEmployee("Kevin", "Dockx");
+            Assert.InRange(employee.Salary, 2500, 3500);
+        }
     }
 }
