5173b6923b40c6b0bf86e8d8435febd673db610c
David Carter <indysoftwaredev@gmail.com>
2023-10-04 18:37:50 -0400

    Adding an assertion on a boolean

---
+++ EmployeeManagement/EmployeeManagement.Test/CourseTests.cs
@@ -0,0 +1,19 @@
+﻿using EmployeeManagement.DataAccess.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Test
+{
+    public class CourseTests
+    {
+        [Fact]
+        public void CourseConstructor_ConstructCourse_IsNewMustBeTrue()
+        {
+            var course = new Course("Disaster Management 101");
+            Assert.True(course.IsNew);
+        }
+    }
+}
--- EmployeeManagement/EmployeeManagement.Test/EmployeeFactoryTests.cs
+++ EmployeeManagement/EmployeeManagement.Test/EmployeeFactoryTests.cs
@@ -11,12 +11,11 @@ namespace EmployeeManagement.Test
     public class EmployeeFactoryTests
     {
         [Fact]
-        public void CreateEmployee_ConstructInternalEMployee_SalaryMustBe2500()
+        public void CreateEmployee_ConstructInternalEmployee_SalaryMustBe2500()
         {
             var employeeFactory = new EmployeeFactory();
             var employee = (InternalEmployee)employeeFactory.CreateEmployee("Kevin", "Dockx");
             Assert.Equal(2500, employee.Salary);
         }
-
     }
 }
