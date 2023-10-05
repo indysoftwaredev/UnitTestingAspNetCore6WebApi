d230e9242f6838fdc18d56030bc87175522d6556
David Carter <indysoftwaredev@gmail.com>
2023-10-05 19:08:03 -0400

    refactoring duplication in EmployeeFactoryTests and demonstrating test class setup by moving the setup to the constructor - less bulky and arcane than [TestSetup], [Before], [BeforeClass] and other methods for this purpose.

--- EmployeeManagement/EmployeeManagement.Test/EmployeeFactoryTests.cs
+++ EmployeeManagement/EmployeeManagement.Test/EmployeeFactoryTests.cs
@@ -8,29 +8,38 @@ using System.Threading.Tasks;
 
 namespace EmployeeManagement.Test
 {
-    public class EmployeeFactoryTests
+    public class EmployeeFactoryTests : IDisposable
     {
+        private EmployeeFactory _employeeFactory;
+
+        public EmployeeFactoryTests()
+        {
+            _employeeFactory = new EmployeeFactory();
+        }
+
+        public void Dispose()
+        {
+            //clean up setup code, if required
+        }
+
         [Fact]
         public void CreateEmployee_ConstructInternalEmployee_SalaryMustBe2500()
         {
-            var employeeFactory = new EmployeeFactory();
-            var employee = (InternalEmployee)employeeFactory.CreateEmployee("Kevin", "Dockx");
+            var employee = (InternalEmployee)_employeeFactory.CreateEmployee("Kevin", "Dockx");
             Assert.Equal(2500, employee.Salary);
         }
 
         [Fact]
         public void CreateEmployee_ConstructInternalEmployee_SalaryMustBeBetween2500And3500()
         {
-            var employeeFactory = new EmployeeFactory();
-            var employee = (InternalEmployee)employeeFactory.CreateEmployee("Kevin", "Dockx");
+            var employee = (InternalEmployee)_employeeFactory.CreateEmployee("Kevin", "Dockx");
             Assert.True(employee.Salary >= 2500 && employee.Salary <= 3500, "Salary not in acceptable range.");
         }
 
         [Fact]
         public void CreateEmployee_ConstructInternalEmployee_SalaryMustBeBetween2500And3500_Alternative()
         {
-            var employeeFactory = new EmployeeFactory();
-            var employee = (InternalEmployee)employeeFactory.CreateEmployee("Kevin", "Dockx");
+            var employee = (InternalEmployee)_employeeFactory.CreateEmployee("Kevin", "Dockx");
             Assert.True(employee.Salary >= 2500, "Salary not in acceptable range.");
             Assert.True(employee.Salary <= 3500, "Salary not in acceptable range.");
         }
@@ -38,16 +47,14 @@ namespace EmployeeManagement.Test
         [Fact]
         public void CreateEmployee_ConstructInternalEmployee_SalaryMustBeBetween2500And3500_AlternativeWithinRange()
         {
-            var employeeFactory = new EmployeeFactory();
-            var employee = (InternalEmployee)employeeFactory.CreateEmployee("Kevin", "Dockx");
+            var employee = (InternalEmployee)_employeeFactory.CreateEmployee("Kevin", "Dockx");
             Assert.InRange(employee.Salary, 2500, 3500);
         }
 
         [Fact]
         public void CreateEmployee_ConstructInternalEmployee_PrecisionExample()
         {
-            var employeeFactory = new EmployeeFactory();
-            var employee = (InternalEmployee)employeeFactory.CreateEmployee("Kevin", "Dockx");
+            var employee = (InternalEmployee)_employeeFactory.CreateEmployee("Kevin", "Dockx");
             employee.Salary = 2500.123m;
             Assert.Equal(2500, employee.Salary, 0);
         }
@@ -55,9 +62,8 @@ namespace EmployeeManagement.Test
         [Fact]
         public void CreateEmployee_IsExternalIsTrue_ReturnTypeMustBeExternalEmployee()
         {
-            var factory = new EmployeeFactory();
 
-            var employee = factory.CreateEmployee("Kevin", "Dockx", "Marvin", true);
+            var employee = _employeeFactory.CreateEmployee("Kevin", "Dockx", "Marvin", true);
 
             Assert.IsType<ExternalEmployee>(employee);
             //Assert.IsAssignableFrom<Employee>(employee);
