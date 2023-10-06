842ed32ae62ef71b3adee1d6f8dab26f5fe4b76a
David Carter <indysoftwaredev@gmail.com>
2023-10-06 13:03:44 -0400

    Adding setup to a Mock, noting that the last parameter match wins.

--- EmployeeManagement/EmployeeManagement.Test/MoqTests.cs
+++ EmployeeManagement/EmployeeManagement.Test/MoqTests.cs
@@ -1,4 +1,5 @@
 ﻿using EmployeeManagement.Business;
+using EmployeeManagement.DataAccess.Entities;
 using EmployeeManagement.Services.Test;
 using Moq;
 using System;
@@ -38,14 +39,42 @@ namespace EmployeeManagement.Test
         {
             var employeeManagementTestDataRepository =
                 new EmployeeManagementTestDataRepository();
+
             var employeeFactoryMock = new Mock<EmployeeFactory>();
+            employeeFactoryMock.Setup(m => 
+                m.CreateEmployee(
+                    "Kevin",
+                    It.IsAny<string>(),
+                    null,
+                    false
+                    ))
+                .Returns(new InternalEmployee("Kevin", "Dockx", 5, 2500, false, 1));
+
+            employeeFactoryMock.Setup(m =>
+                m.CreateEmployee(
+                    "Sandy",
+                    It.IsAny<string>(),
+                    null,
+                    false
+                    ))
+                .Returns(new InternalEmployee("Kevin", "Dockx", 5, 2500, false, 1));
+
+            employeeFactoryMock.Setup(m =>
+                m.CreateEmployee(
+                    It.Is<string>(value => value.Contains("a")),
+                    It.IsAny<string>(),
+                    null,
+                    false
+                    ))
+                .Returns(new InternalEmployee("SomeoneWithAna", "Dockx", 5, 2500, false, 1));
+
             var employeeService = new EmployeeService(
                 employeeManagementTestDataRepository, employeeFactoryMock.Object);
 
-            decimal suggestedBonus = 200;
+            decimal suggestedBonus = 1000;
 
             //Act
-            var employee = employeeService.CreateInternalEmployee("Kevin", "Dockx");
+            var employee = employeeService.CreateInternalEmployee("Sandy", "Dockx");
 
             //Assert
             Assert.Equal(suggestedBonus, employee.SuggestedBonus);
